494fe212099e187252cc446f016d7c70ebcbd6a9
Eddh <remi.dufour@protonmail.com>
2020-02-12 01:24:13 +0100

    archipelagos map type

--- core/src/com/unciv/logic/map/MapGenerator.kt
+++ core/src/com/unciv/logic/map/MapGenerator.kt
@@ -564,6 +564,7 @@ class MapGenerator(val ruleset: Ruleset) {
                 MapType.pangaea -> createPangea(tileMap)
                 MapType.continents -> createTwoContinents(tileMap)
                 MapType.perlin -> createPerlin(tileMap)
+                MapType.archipelago -> createArchipelago(tileMap)
                 MapType.default -> generateLandCellularAutomata(tileMap)
             }
         }
@@ -585,6 +586,18 @@ class MapGenerator(val ruleset: Ruleset) {
             val elevationSeed = RNG.nextInt().toDouble()
             for (tile in tileMap.values) {
                 var elevation = getPerlinNoise(tile, elevationSeed)
+
+                when {
+                    elevation < 0 -> tile.baseTerrain = Constants.ocean
+                    else -> tile.baseTerrain = Constants.grassland
+                }
+            }
+        }
+
+        private fun createArchipelago(tileMap: TileMap) {
+            val elevationSeed = RNG.nextInt().toDouble()
+            for (tile in tileMap.values) {
+                var elevation = getRidgedPerlinNoise(tile, elevationSeed)-0.25
 
                 when {
                     elevation < 0 -> tile.baseTerrain = Constants.ocean
@@ -651,6 +664,15 @@ class MapGenerator(val ruleset: Ruleset) {
                                    scale: Double = 10.0): Double {
             val worldCoords = HexMath.hex2WorldCoords(tile.position)
             return Perlin.noise3d(worldCoords.x.toDouble(), worldCoords.y.toDouble(), seed, nOctaves, persistence, lacunarity, scale)
+        }
+
+        private fun getRidgedPerlinNoise(tile: TileInfo, seed: Double,
+                                         nOctaves: Int = 10,
+                                         persistence: Double = 0.5,
+                                         lacunarity: Double = 2.0,
+                                         scale: Double = 15.0): Double {
+            val worldCoords = HexMath.hex2WorldCoords(tile.position)
+            return Perlin.ridgedNoise3d(worldCoords.x.toDouble(), worldCoords.y.toDouble(), seed, nOctaves, persistence, lacunarity, scale)
         }
 
         // region Cellular automata
